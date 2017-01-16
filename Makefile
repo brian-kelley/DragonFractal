47a0285ecf949dfb796d1a5d5441ef318ec8c7bb
Brian Kelley <brian.honda11@gmail.com>
2017-01-15 21:42:25 -0700

    Added blending, partial path and dense options

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gcc -O3 -Wall lodepng.c dragon.c -o dragon.exe
+	gcc -O3 -std=c11 -Wall lodepng.c dragon.c -o dragon.exe
 
 clean:
 	rm dragon.exe
--- dragon.c
+++ dragon.c
@@ -1,5 +1,7 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
+#include "stdbool.h"
 #include "lodepng.h"
 
 typedef unsigned char Pixel;
@@ -21,13 +23,28 @@ enum DIRECTION
 
 Pixel* buf;
 Turn* turnBuf;
+unsigned BLACK = 0x000000;
+unsigned WHITE = 0xFFFFFF;
+unsigned blend1;
+unsigned blend2;
+bool dense;
 int iters;
 int maxTurns;
+double partial;
 int w;    //image width
 int h;    //image height
 
 int cx;   //center x (axis of rotation)
 int cy;   //center y
+
+unsigned lerp(unsigned c1, unsigned c2, double k)
+{
+  //rgba -> abgr, a omitted from a, b
+  unsigned r = (c1 & 0xFF) * (1 - k) + (c2 & 0xFF) * k;
+  unsigned g = ((c1 & 0xFF00) >> 8) * (1 - k) + ((c2 & 0xFF00) >> 8) * k;
+  unsigned b = ((c1 & 0xFF0000) >> 16) * (1 - k) + ((c2 & 0xFF0000) >> 16) * k;
+  return 0xFF000000 | (b << 16) | (g << 8) | r;
+}
 
 //precondition: turnBuf is populated
 void createImage()
@@ -81,20 +98,18 @@ void createImage()
   //using the bounding box, compute final image size
   //note: in real image, each position update is 2 pixels
   //also add one pixel border on all 4 edges
-  int iw = (maxx - minx) * 2 + 3;
-  int ih = (maxy - miny) * 2 + 3;
+  int iw = (maxx - minx) * (dense ? 1 : 2) + 3;
+  int ih = (maxy - miny) * (dense ? 1 : 2) + 3;
   unsigned* pixels = malloc(iw * ih * sizeof(unsigned));
-  const unsigned BLACK = 0xFF000000;
-  const unsigned WHITE = 0xFFFFFFFF;
   //fill image with BLACK (most pixels will be in final product)
   for(int i = 0; i < iw * ih; i++)
-    pixels[i] = BLACK;
+    pixels[i] = (0xFF000000 | BLACK);
   //set starting point to stay in [0, iw) x [0, ih)
-  x = (-minx) * 2 + 1;
-  y = (-miny) * 2 + 1;
+  x = (-minx) * (dense ? 1 : 2) + 1;
+  y = (-miny) * (dense ? 1 : 2) + 1;
   dir = D_UP;
-#define SET_PIXEL(x, y) {pixels[(x) + (y) * iw] = WHITE;}
-  for(int i = 0; i <= maxTurns; i++)
+#define SET_PIXEL(x, y) {pixels[(x) + (y) * iw] = lerp(blend1, blend2, (double) i / maxTurns);} 
+  for(int i = 0; i <= maxTurns * partial; i++)
   {
     //move in current direction, writing pixels on segment to WHITE
     SET_PIXEL(x, y);
@@ -102,19 +117,19 @@ void createImage()
     {
       case D_LEFT:
         SET_PIXEL(x - 1, y);
-        x -= 2;
+        x -= (dense ? 1 : 2);
         break;
       case D_UP:
         SET_PIXEL(x, y - 1);
-        y -= 2;
+        y -= (dense ? 1 : 2);
         break;
       case D_RIGHT:
         SET_PIXEL(x + 1, y);
-        x += 2;
+        x += (dense ? 1 : 2);
         break;
       case D_DOWN:
         SET_PIXEL(x, y + 1);
-        y += 2;
+        y += (dense ? 1 : 2);
         break;
       default:;
     }
@@ -166,17 +181,66 @@ void getPath()
 
 int main(int argc, const char** argv)
 {
+  //set default option values
   iters = 10;
-  if(argc == 2)
+  dense = false;
+  blend1 = WHITE;
+  blend2 = WHITE;
+  partial = 1.0;
+  int optStart = 0;
+  if(argc >= 2)
   {
     int newIters;
     if(1 == sscanf(argv[1], "%i", &newIters) && newIters >= 0)
     {
       iters = newIters;
+      optStart = 2;
     }
     else
     {
-      puts("Invalid iteration count, defaulting to 10.");
+      puts("Defaulting to 10 iterations.");
+      optStart = 1;
+    }
+  }
+  for(int arg = optStart; arg < argc; arg++)
+  {
+    if(strcmp(argv[arg], "--dense") == 0)
+    {
+      dense = true;
+    }
+    else if(strcmp(argv[arg], "--blend") == 0)
+    {
+      if(argc >= arg + 2)
+      {
+        unsigned b1, b2;
+        if(1 != sscanf(argv[arg + 1], "%x", &b1))
+          puts("invalid first blend color, defaulting to white.");
+        else
+          blend1 = (b1 & 0xFF0000) >> 16 | (b1 & 0xFF00) | (b1 & 0xFF) << 16;
+        if(1 != sscanf(argv[arg + 2], "%x", &b2))
+          puts("invalid second blend color, defaulting to white.");
+        else
+          blend2 = (b2 & 0xFF0000) >> 16 | (b2 & 0xFF00) | (b2 & 0xFF) << 16;
+      }
+      else
+        puts("Must follow --blend flag with two colors in the form 0xRRGGBB.");
+      arg += 2;
+    }
+    else if(strcmp(argv[arg], "--partial") == 0)
+    {
+      if(arg == argc - 1)
+      {
+        puts("Must follow --partial option with a floating-point number between 0 and 1.");
+        puts("Defaulting to 1.");
+      }
+      else
+      {
+        double p;
+        if(1 != sscanf(argv[arg + 1], "%lf", &p) || p < 0 || p > 1)
+          puts("Invalid partial path value, must be a floating-point number between 0 and 1.");
+        else
+          partial = p;
+      }
     }
   }
   //use 1 byte per pixel, 1 = white, 0 = black
